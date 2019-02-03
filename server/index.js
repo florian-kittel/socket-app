37bd2cb97263fd80835e60d6f3546eacf0f1ac8c
florian-kittel <mail@florian-kittel.de>
2019-02-03 15:04:49 +0100

    feat(server):  add rooms and refresh

--- server/index.js
+++ server/index.js
@@ -4,21 +4,43 @@ const socketIO = require('socket.io');
 
 // our localhost port
 const port = 4001;
-
 const app = express();
 
 // our server instance
 const server = http.createServer(app);
 
 // This creates our socket using the instance of the server
-const io = socketIO(server);
+const io = socketIO(server, {
+  path: '/api/mysocket'
+});
 
 // This is what the socket.io syntax is like, we will work this later
 io.on('connection', socket => {
-  console.log('Connected client on port %s.', port);
+  console.log('Connected client on port %s with ID %s', port, socket.id);
+
   socket.on('message', (m) => {
-    console.log('[server](message): %s', JSON.stringify(m));
-    io.emit('message', m);
+    // console.log('[server][%s](message): %s', socket.id, JSON.stringify(m));
+    io.in(m.room || 'default').emit('message', m);
+  });
+
+  socket.on('room', (newRoom) => {
+    let rooms = io.sockets.adapter.sids[socket.id];
+
+    for (var room in rooms) {
+      if (room !== socket.id) {
+        socket.leave(room);
+        console.log(socket.id, 'leaves room', room);
+      }
+    }
+
+    console.log(socket.id, 'joins room', newRoom);
+    socket.join(newRoom);
+  });
+
+  socket.on('reload', (room) => {
+    socket.to(room || 'default').emit('reload', room);
+    // Refresh all other and ignore rooms
+    // socket.broadcast.emit('reload', m);
   });
 
   socket.on('disconnect', () => {
@@ -26,4 +48,4 @@ io.on('connection', socket => {
   });
 });
 
-server.listen(port, () => console.log(`Listening on port ${port}`))
+server.listen(port, () => console.log(`Listening on port ${port}`));
